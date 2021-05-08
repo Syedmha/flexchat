5da2587720bbe0f7981d81bfdc3961e6ce4e118d
Syedmha <syedabedi12@gmail.com>
2021-05-09 00:35:50 +0530

    Favicon updated

--- src/components/ChatLIst/ChatList.js
+++ src/components/ChatLIst/ChatList.js
@@ -2,30 +2,35 @@ import React from 'react';
 import './ChatList.css';
 import { auth, firestore } from './../../firebase/config';
 import firebase from './../../firebase/config';
+import { useList } from 'react-firebase-hooks/database';
 
 
 const ChatList = () => {
 
   const { uid, photoURL, displayName } = auth.currentUser;
 
-  const db = firestore.collection("users")
-    .onSnapshot((querySnapshot) => {
-        var user = [];
-        querySnapshot.forEach((doc) => {
-            user.add({
+  // const DatabaseList = () => {
+  //   const [] = useList(firebase.database().ref('messages'));
+  // }
+  
+
+  // const db = firestore.collection("users")
+  //   .onSnapshot((querySnapshot) => {
+  //       var user = [];
+  //       querySnapshot.forEach((doc) => {
+  //           user.add({
               
-              createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-              uid,
-              photoURL,
-              displayName
-            });
-        });
-        console.log("Current user: ", displayName);
-    });
+  //             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  //             uid,
+  //             photoURL,
+  //             displayName
+  //           });
+  //       });
+  //       console.log("Current user: ", displayName);
+  //   });
 
     return(
-        <div className="main__chatlist">
-          {db}
+      <div className="main__chatlist">
         <button className="btn">
           <i className="fa fa-plus"></i>
           <span>New conversation</span>
@@ -44,9 +49,7 @@ const ChatList = () => {
             </button>
           </div>
         </div>
-            
-        </div>
+      </div>
     )
-};
-
+}
 export default ChatList;
